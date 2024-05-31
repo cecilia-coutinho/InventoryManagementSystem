b97956a2805d041ae280a4887afe729a35760a6d
cecilia.coutinho <cecilia.coutinho@outlook.com>
2024-05-31 20:28:39 +0200

    Applying Specification Pattern Create: ISpecification and implementation

---
+++ Backend/InventorySystemAPI/Specifications/BaseSpecification.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq.Expressions;
+
+namespace InventorySystemAPI.Specifications
+{
+    public abstract class BaseSpecification<T> : ISpecification<T>
+    {
+        public Expression<Func<T, bool>>? Criteria { get; }
+        protected BaseSpecification(Expression<Func<T, bool>>? criteria)
+        {
+            Criteria = criteria;
+        }
+        public BaseSpecification() 
+        { 
+        }
+
+        public List<Expression<Func<T, object>>>? Includes { get; } = new List<Expression<Func<T, object>>>();
+        public Expression<Func<T, object>>? OrderBy { get; private set; }
+        public Expression<Func<T, object>>? OrderByDescending { get; private set; }
+        public Expression<Func<T, object>>? GroupBy { get; private set; }
+
+        public int? Take { get; private set; }
+        public int? Skip { get; private set; }
+        public bool IsPagingEnabled { get; private set; } = false;
+
+        protected void AddInclude(Expression<Func<T, object>> includeExpression)
+        {
+            Includes?.Add(includeExpression);
+        }
+        protected void ApplyPaging(int skip, int take)
+        {
+            Skip = skip;
+            Take = take;
+            IsPagingEnabled = true;
+        }
+
+        protected void ApplyOrderBy(Expression<Func<T, object>> orderByExpression)
+        {
+            OrderBy = orderByExpression;
+        }
+
+        protected void ApplyOrderByDescending(Expression<Func<T, object>> orderByDescExpression)
+        {
+            OrderByDescending = orderByDescExpression;
+        }
+
+
+        protected void ApplyGroupBy(Expression<Func<T, object>> groupByExpression)
+        {
+            GroupBy = groupByExpression;
+        }
+
+
+    }
+}
