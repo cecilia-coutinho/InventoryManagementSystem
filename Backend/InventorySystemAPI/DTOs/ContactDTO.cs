5dd958609d6c82ab56c6ebe7d06bab6b7fed4097
cecilia.coutinho <cecilia.coutinho@outlook.com>
2024-05-31 20:33:49 +0200

    Create: ContactDto class for data handling

---
+++ Backend/InventorySystemAPI/DTOs/ContactDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InventorySystemAPI.DTOs
+{
+    public class ContactDTO
+    {
+        [Required]
+        [StringLength(50)]
+        public string? FirstName { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string? LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+    }
+}
