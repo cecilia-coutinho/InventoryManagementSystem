d67974d7d3c7696ac342bfdc0c074c1e917ad3dd
cecilia.coutinho <cecilia.coutinho@outlook.com>
2024-06-05 00:35:05 +0200

    Refactored: Rules added and styles polishing

--- Frontend/inventory-system-client/pages/products.vue
+++ Frontend/inventory-system-client/pages/products.vue
@@ -15,10 +15,10 @@
             <table class="table-list">
                 <thead>
                     <tr>
-                        <th v-for="header in tableHeaders" :key="header">
+                        <th v-for="header in tableHeaders" :key="header" style="text-align: center;">
                             {{ header }}
                         </th>
-                        <th>A��es</th>
+                        <th style="width: 150px; text-align: center;">Ações</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -27,18 +27,29 @@
                         <td>{{ p.productDescription }}</td>
                         <td>{{ p.sellPrice }}</td>
                         <td>{{ p.costPrice }}</td>
-                        <td>{{ p.fkProductCategory }}</td>
                         <td>
-                            <v-icon small class="mr-2" @click="openEditProductDialog(p.id, p, edit, refresh)">mdi-pencil</v-icon>
-                            <v-icon small class="mr-2" @click="openProductDetailsDialog(p.id, p)">mdi-magnify</v-icon>
-                            <v-icon small class="mr-2" @click="deleteProduct(p.id, deleteItem, refresh)">mdi-delete</v-icon>
+                            <DataFetcher :uri="`${apiCategoryUri}/${p.fkProductCategory}`">
+                                <template #default="{ data: categoryData }">
+                                    {{ categoryData.productCategoryName }}
+
+                                </template>
+                            </DataFetcher>
+                        </td>
+                        <td>
+                            <v-icon color="blue darken-1" small class="mr-2" style="margin: 8px;"
+                                @click="openEditProductDialog(p.id, p, edit, refresh)">mdi-pencil</v-icon>
+                            <v-icon color="#4caf50" small class="mr-2" style="margin: 8px;"
+                                @click="openProductDetailsDialog(p.id, p)">mdi-magnify</v-icon>
+                            <v-icon color="#ff5252" small class="mr-2" style="margin: 8px;"
+                                @click="deleteProduct(p.id, deleteItem, refresh)">mdi-delete</v-icon>
                         </td>
                     </tr>
                 </tbody>
             </table>
             <div class="add-btn">
                 <v-row class="add-btn">
-                    <v-btn color="#4caf50" @click="openAddProductDialog" style="margin-bottom: 2rem; margin-left: 1rem;">
+                    <v-btn color="#4caf50" @click="openAddProductDialog"
+                        style="margin-bottom: 2rem; margin-left: 1rem;">
                         <v-icon>mdi-plus</v-icon> Adicionar Produto
                     </v-btn>
                 </v-row>
@@ -52,26 +63,34 @@
                         <v-container>
                             <v-row>
                                 <v-col cols="12">
-                                    <v-text-field v-model="editedProduct.productName" label="Nome do Produto"></v-text-field>
+                                    <v-text-field v-model="editedProduct.productName" :rules="productNameRules"
+                                        label="Nome do Produto"></v-text-field>
                                 </v-col>
                                 <v-col cols="12">
-                                    <v-text-field v-model="editedProduct.productDescription" label="Descri��o do Produto"></v-text-field>
+                                    <v-text-field v-model="editedProduct.productDescription"
+                                        label="Descrição do Produto"></v-text-field>
                                 </v-col>
                                 <v-col cols="12">
-                                    <v-text-field v-model="editedProduct.sellPrice" label="Pre�o de Venda"></v-text-field>
+                                    <v-text-field v-model="editedProduct.sellPrice" :rules="sellPriceRules"
+                                        label="Preço de Venda" placeholder="Use ponto (.) para separar os centavos">
+                                    </v-text-field>
                                 </v-col>
                                 <v-col cols="12">
-                                    <v-text-field v-model="editedProduct.costPrice" label="Pre�o de Compra"></v-text-field>
+                                    <v-text-field v-model="editedProduct.costPrice" :rules="costPriceRules"
+                                        label="Preço de Compra" placeholder="Use ponto (.) para separar os centavos">
+                                    </v-text-field>
                                 </v-col>
                                 <v-col cols="12">
-                                    <v-text-field v-model="editedProduct.fkProductCategory" label="Categoria do Produto"></v-text-field>
+                                    <v-text-field v-model="editedProduct.fkProductCategory"
+                                        :rules="productCategoryRules" label="Categoria do Produto"></v-text-field>
                                 </v-col>
                             </v-row>
                         </v-container>
                     </v-card-text>
                     <v-card-actions>
                         <v-btn color="blue darken-1" text @click="close">Fechar</v-btn>
-                        <v-btn color="#4caf50" text @click="() => save(create, edit, refresh)" v-if="!isDetailsDialog">Salvar</v-btn>
+                        <v-btn color="#4caf50" text @click="() => save(create, edit, refresh)"
+                            v-if="!isDetailsDialog">Salvar</v-btn>
                     </v-card-actions>
                 </v-card>
             </v-dialog>
@@ -82,123 +101,144 @@
 </template>
 
 <script setup>
-    import { ref } from 'vue';
+import { ref } from 'vue';
 
-    const tableHeaders = [
-        'Producto',
-        'Descri��o',
-        'Pre�o de Venda',
-        'Pre�o de Compra',
-        'Categoria',
-    ];
+const tableHeaders = [
+    'Producto',
+    'Descrição',
+    'Preço de Venda',
+    'Preço de Compra',
+    'Categoria',
+];
 
-    const dialog = ref(false);
-    const dialogTitle = ref('');
-    const isDetailsDialog = ref(false);
-    const currentProductId = ref(null);
+const productNameRules = computed(() => [
+    v => !!v || 'Nome do Produto é obrigatório',
+    v => (v && v.length <= 100) || 'Nome do Produto deve ter no máximo 100 caracteres'
+]);
 
-    const editedProduct = ref({
+const sellPriceRules = computed(() => [
+    v => !!v || 'Preço de Venda é obrigatório',
+    v => (v && !isNaN(v)) || 'Preço de Venda deve ser um número e use ponto (.) para separar os centavos'
+]);
+
+const costPriceRules = computed(() => [
+    v => !!v || 'Preço de Compra é obrigatório',
+    v => (v && !isNaN(v)) || 'Preço de Compra deve ser um número e use ponto (.) para separar os centavos'
+]);
+
+const productCategoryRules = computed(() => [
+    v => !!v || 'Categoria do Produto é obrigatório'
+]);
+
+const dialog = ref(false);
+const dialogTitle = ref('');
+const isDetailsDialog = ref(false);
+const currentProductId = ref(null);
+
+const editedProduct = ref({
+    productName: '',
+    productDescription: '',
+    sellPrice: 0,
+    costPrice: 0,
+    productCategory: '',
+});
+
+const apiBaseUri = 'https://localhost:7171/api/Products';
+const apiCategoryUri = 'https://localhost:7171/api/ProductCategories';
+
+const openAddProductDialog = () => {
+    dialog.value = true;
+    dialogTitle.value = 'Adicionar Novo Produto';
+    editedProduct.value = {
         productName: '',
         productDescription: '',
         sellPrice: 0,
         costPrice: 0,
-        productCategory: '',
-    });
-
-    const apiBaseUri = 'https://localhost:7171/api/Products';
-
-    const openAddProductDialog = () => {
-        dialog.value = true;
-        dialogTitle.value = 'Adicionar Novo Produto';
-        editedProduct.value = {
-            productName: '',
-            productDescription: '',
-            sellPrice: 0,
-            costPrice: 0,
-            fkProductCategory: '',
-        };
+        fkProductCategory: '',
     };
+};
 
-    const openEditProductDialog = (id, product) => {
-        dialogTitle.value = 'Editar Produto';
-        editedProduct.value = { ...product };
-        currentProductId.value = id;
-        dialog.value = true;
-    };
+const openEditProductDialog = (id, product) => {
+    dialogTitle.value = 'Editar Produto';
+    editedProduct.value = { ...product };
+    currentProductId.value = id;
+    dialog.value = true;
+};
 
-    const openProductDetailsDialog = (id, product) => {
-        dialogTitle.value = 'Detalhes do Produto';
-        editedProduct.value = { ...product };
-        currentProductId.value = id;
-        dialog.value = true;
-        isDetailsDialog.value = true;
-    };
+const openProductDetailsDialog = (id, product) => {
+    dialogTitle.value = 'Detalhes do Produto';
+    editedProduct.value = { ...product };
+    currentProductId.value = id;
+    dialog.value = true;
+    isDetailsDialog.value = true;
+};
 
-    const deleteProduct = async (id, deleteItem, refresh) => {
-        await deleteItem(id);
-        refresh();
-    };
+const deleteProduct = async (id, deleteItem, refresh) => {
+    await deleteItem(id);
+    refresh();
+};
 
-    const close = () => {
-        dialog.value = false;
-        isDetailsDialog.value = false;
-    };
+const close = () => {
+    dialog.value = false;
+    isDetailsDialog.value = false;
+};
 
-    const save = async (createFunction, editFunction, refresh) => {
-        if (currentProductId.value) {
-            await editFunction(currentProductId.value, editedProduct.value);
-        } else {
-            await createFunction(editedProduct.value);
-        }
-        dialog.value = false;
-        close();
-        refresh();
-    };
+const save = async (createFunction, editFunction, refresh) => {
+    if (currentProductId.value) {
+        await editFunction(currentProductId.value, editedProduct.value);
+    } else {
+        await createFunction(editedProduct.value);
+    }
+    dialog.value = false;
+    close();
+    refresh();
+};
 </script>
 
 <style scoped>
-    table {
-        border-collapse: collapse;
-        width: 100%;
-    }
+table {
+    border-collapse: collapse;
+    width: 100%;
+}
 
-    th, td {
-        border: 1px solid #4caf50;
-        padding: 8px;
-        text-align: left;
-    }
+th,
+td {
+    border: 1px solid #4caf50;
+    padding: 8px;
+    text-align: left;
+}
 
-    th {
-        background-color: #16161d;
-        color: #fec859;
-        font-weight: 200;
-    }
+th {
+    background-color: #16161d;
+    color: #fec859;
+    font-weight: 200;
+}
 
-    .mdi-icon {
-        color: #4caf50;
-    }
+.mdi-icon {
+    color: #4caf50;
+}
 
-    .col-span-1 {
-        font-size: 14px;
-        padding: .2rem 1rem;
-    }
+.col-span-1 {
+    font-size: 14px;
+    padding: .2rem 1rem;
+}
 
-    .list-title {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        margin: 1rem;
-    }
+.list-title {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 1rem;
+}
 
-    .add-btn {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        margin-top: 1.5rem;
-    }
+.add-btn {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 1.5rem;
+}
 
-    .table-list {
-        max-width: 90%;
-        margin: 0 auto;
-    }
+.table-list {
+    max-width: 90%;
+    margin: 0 auto;
+}
 </style>
