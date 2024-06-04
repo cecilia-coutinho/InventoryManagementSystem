cae5e852025a75aab304304fec5c691e87c22cb4
cecilia.coutinho <cecilia.coutinho@outlook.com>
2024-06-04 14:30:07 +0200

    WarningFix: solving warning for NuxtPwaAssets

--- Frontend/inventory-system-client/app.vue
+++ Frontend/inventory-system-client/app.vue
@@ -1,8 +1,7 @@
 <template>
-  <div>
-    <NuxtPwaAssets />
-    <NuxtLayout>
-      <NuxtPage />
-    </NuxtLayout>
-  </div>
+    <div>
+        <NuxtLayout>
+            <NuxtPage />
+        </NuxtLayout>
+    </div>
 </template>
