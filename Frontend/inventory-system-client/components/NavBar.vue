caeb93db58d793301d2a9399ed853049a1ae86ed
cecilia.coutinho <cecilia.coutinho@outlook.com>
2024-06-06 14:56:26 +0200

    Refactor: nav drawer added

--- Frontend/inventory-system-client/components/NavBar.vue
+++ Frontend/inventory-system-client/components/NavBar.vue
@@ -1,17 +1,51 @@
 <template>
+    <v-navigation-drawer v-model="drawer" app class="custom-nav-drawer">
+        <v-list nav>
+            <v-list-item :to="{ path: '/' }" prepend-icon="mdi-view-dashboard">
+                Dashboard
+            </v-list-item>
+            <v-list-item :to="{ path: '/categories' }" prepend-icon="mdi-folder-outline">
+                Categorias
+            </v-list-item>
+            <v-list-item :to="{ path: '/products' }" prepend-icon="mdi-cart-outline">
+                Productos
+            </v-list-item>
+            <v-list-item :to="{ path: '/inventory' }" prepend-icon="mdi-package-variant-closed">
+                Inventário
+            </v-list-item>
+            <v-list-item :to="{ path: '/suppliers' }" prepend-icon="mdi-truck-outline">
+                Fornecedores
+            </v-list-item>
+        </v-list>
+    </v-navigation-drawer>
+
     <v-app-bar app style="background-color: #16161d; color: #bb86fc">
+        <v-app-bar-nav-icon @click="toggleDrawer"></v-app-bar-nav-icon>
         <v-toolbar-title>Sistema de Inventário</v-toolbar-title>
         <v-spacer></v-spacer>
-        <v-btn text :to="{ path: '/' }">Home</v-btn>
-        <v-btn text :to="{ path: '/categories' }">Categorias</v-btn>
-        <v-btn text :to="{ path: '/products' }">Productos</v-btn>
-        <v-btn text :to="{ path: '/inventory' }">Inventário</v-btn>
-        <v-btn text :to="{ path: '/suppliers' }">Fornecedores</v-btn>
     </v-app-bar>
 </template>
 
 <script>
-export default {
-    name: 'NavBar',
-}
+    export default {
+        name: 'NavBar',
+        data() {
+            return {
+                drawer: false,
+            };
+        },
+        methods: {
+            toggleDrawer() {
+                this.drawer = !this.drawer;
+            },
+        },
+    };
 </script>
+
+<style scoped>
+    .custom-nav-drawer {
+        background-color: #16161d;
+        color: #bb86fc;
+        padding: 20px;
+    }
+</style>
