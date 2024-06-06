aeb50ca73d22a2be032ee16a1394e667e240ecba
cecilia.coutinho <cecilia.coutinho@outlook.com>
2024-06-06 15:29:51 +0200

    Refactor: v-list-item title added, boilerplate removed

--- Frontend/inventory-system-client/components/NavBar.vue
+++ Frontend/inventory-system-client/components/NavBar.vue
@@ -1,28 +1,17 @@
 <template>
     <v-navigation-drawer v-model="drawer" app class="custom-nav-drawer">
         <v-list nav>
-            <v-list-item :to="{ path: '/' }" prepend-icon="mdi-view-dashboard">
-                Dashboard
-            </v-list-item>
-            <v-list-item :to="{ path: '/categories' }" prepend-icon="mdi-folder-outline">
-                Categorias
-            </v-list-item>
-            <v-list-item :to="{ path: '/products' }" prepend-icon="mdi-cart-outline">
-                Productos
-            </v-list-item>
-            <v-list-item :to="{ path: '/inventory' }" prepend-icon="mdi-package-variant-closed">
-                Inventário
-            </v-list-item>
-            <v-list-item :to="{ path: '/suppliers' }" prepend-icon="mdi-truck-outline">
-                Fornecedores
-            </v-list-item>
+            <v-list-item :to="{ path: '/' }" prepend-icon="mdi-view-dashboard" title="Dashboard" />
+            <v-list-item :to="{ path: '/categories' }" prepend-icon="mdi-folder-outline" title="Categorias" />
+            <v-list-item :to="{ path: '/products' }" prepend-icon="mdi-cart-outline" title="Productos" />
+            <v-list-item :to="{ path: '/inventory' }" prepend-icon="mdi-package-variant-closed" title="Inventário" />
+            <v-list-item :to="{ path: '/suppliers' }" prepend-icon="mdi-truck-outline" title="Fornecedores" />
         </v-list>
     </v-navigation-drawer>
 
     <v-app-bar app style="background-color: #16161d; color: #bb86fc">
         <v-app-bar-nav-icon @click="toggleDrawer"></v-app-bar-nav-icon>
-        <v-toolbar-title>Sistema de Inventário</v-toolbar-title>
-        <v-spacer></v-spacer>
+        <v-toolbar-title title="Sistema de Inventário">Sistema de Inventário</v-toolbar-title>
     </v-app-bar>
 </template>
 
